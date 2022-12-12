b0e6973158368033b156a8831fce9c3d6b6f484c
Kevin <kelvinngechu@gmail.com>
2022-12-12 20:42:55 +0300

    updated assignment

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
+function callBack(){
+  return 'This is a function callback'
+}
 
-const drivers = ['Ian', 'Ryan', 'Brian', 'Dennis'];
+function receivesAFunction(callBack){
+  callBack();
+}
 
-const returnFirstTwoDrivers = (drivers) => {
-    return [drivers[0], drivers[1]];
-  };
- 
-  const returnLastTwoDrivers = (drivers) => {
-    return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
-  };
-  
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-
-const createFareMultiplier = (multiplier) => {
-  return function () {
-    return multiplier ** 2;
-  };
-};
-const fareDoubler = (fare) => {
-    return fare * 2;
-  };
-  
-  const fareTripler = (fare) => fare * 3;
-  
-  function selectDifferentDrivers(arrayOfDrivers, func) {
-    return func(arrayOfDrivers);
+function returnsANamedFunction(){
+  return function fn(){
+      'This returns a named function'
   }
-  /*const fareDoubler = (fare) => {
-    return fare * 2;
-  };
-  
-  const fareTripler = (fare) => fare * 3;
-  
-  function selectDifferentDrivers(arrayOfDrivers, func) {
-    return func(arrayOfDrivers);
-  }
+}
 
+function returnsAnAnonymousFunction(){
+  return function(){
+      'An anonymous function is returned'
   }
-  
-  const selectDifferentDrivers = function (drivers, otherDrivers)  {
-   return otherDrivers(drivers);
-  }*/
-
-  
+}
